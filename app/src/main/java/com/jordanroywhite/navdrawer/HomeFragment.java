80c6d86f4c85bed7bbed0f3afac997cf8b223a84
Jordan White <jordanrw@vt.edu>
2017-04-22 20:53:56 -0400

    Initial commit

---
+++ app/src/main/java/com/jordanroywhite/navdrawer/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.jordanroywhite.navdrawer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Jordan on 4/22/17.
+ */
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    TextView label;
+
+    public void HomeFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        label = (TextView) view.findViewById(R.id.homeTitle);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+}
